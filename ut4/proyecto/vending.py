518530f1f0d699e3ae8ee81335d87e66b9fef249
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2023-02-23 22:03:18 +0000

    Se suben pruebas del proyecto

--- ut4/proyecto/vending.py
+++ ut4/proyecto/vending.py
@@ -1,3 +1,22 @@
-with open("/files/vending.dat") as f: 
-    money = f.readline()
-    money
+with open("files/vending.dat") as f: 
+    money = f.readline().strip().split()
+    money = [int(value) for value in money]
+
+def final_dat(money: list): 
+    with open("files/final.dat", "w") as f:
+        f.write(" ".join([str(value) for value in money]))
+
+def data():
+    with open("files/final.dat") as f: 
+        money = f.readline().strip().split()
+        return [int(value) for value in money]
+    
+
+uno = 5
+dos = 2
+tres = 1
+
+
+
+
+
