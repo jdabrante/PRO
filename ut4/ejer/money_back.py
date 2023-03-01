203e5e4c1a0c4260619db654038d2556ef6c1b36
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2023-03-01 19:25:12 +0000

    Se sube lo hecho en clase de pro

--- ut4/ejer/money_back.py
+++ ut4/ejer/money_back.py
@@ -8,13 +8,13 @@ def run(to_give_back: float, available_money_currencies: list) -> dict:
     money_back = {}
     if to_give_back:
         for available_money in money_currencies:
-            money_back[available_money] = to_give_back//available_money
-            to_give_back = to_give_back%available_money
-            if to_give_back == 0: 
+            money_back[available_money] = to_give_back // available_money
+            to_give_back = to_give_back % available_money
+            if to_give_back == 0:
                 return money_back
-    else: 
+    else:
         return money_back
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run(20, [5, 2, 1])
--- ut4/ejer/money_back_max.py
+++ ut4/ejer/money_back_max.py
@@ -2,27 +2,25 @@
 # AQU� TIENE SU VUELTA (MAX)
 # **************************
 
-def d_reverse(d: dict)-> dict:
-    lt_us = [(key,value) for key,value in d.items()]
-    d_s = dict(sorted(lt_us, reverse=True))
-    return d_s
+
+def d_reverse(d: dict) -> dict:
+    unsorted_tuple_list = [(key, value) for key, value in d.items()]
+    sorted_dict = dict(sorted(unsorted_tuple_list, reverse=True))
+    return sorted_dict
+
 
 def run(to_give_back: float, available_currencies: dict) -> dict:
     order_available_currencies = d_reverse(available_currencies)
     money_back = {}
-    if to_give_back: 
-        for money,amount in order_available_currencies.items():
-            money_back[money] = to_give_back//money 
-            if money_back[money] > amount:
-                money_back[money] = amount
-                order_available_currencies[money] = 0
-            order_available_currencies[money] = amount - money_back[money]
+    for money, amount in order_available_currencies.items():
+        given_money = to_give_back // money
+        if given_money != 0:
+            money_back[money] = min(given_money, amount)
             to_give_back -= money_back[money] * money
-            if to_give_back == 0:
-                return money_back
-    else: 
+    if to_give_back == 0:
         return money_back
+    return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run(20, {5: 3, 2: 7, 1: 3})
