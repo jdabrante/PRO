a488e3807111754d9e2d5975111204765901e524
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2023-03-03 18:28:20 +0000

    Se sube

--- ut4/reproyecto/stock_price.py
+++ ut4/reproyecto/stock_price.py
@@ -1,19 +1,20 @@
 def price_rest(operation: list, products: dict) -> dict:
+
     operation_type = operation[0]
     product_code = operation[1]
     target_feature = int(operation[2])
     if operation_type == "R":
         target = "stock"
-    else: 
+    else:
         target = "price"
     if product_code in products:
         product = products[product_code]
         if operation_type == "R":
             product[target] += target_feature
-        else: 
+        else:
             product[target] = target_feature
-    elif product_code not in products and operation_type == "R": 
-        stock_price = {"stock": 0,"price": 0}
+    elif product_code not in products and operation_type == "R":
+        stock_price = {"stock": 0, "price": 0}
         stock_price[target] = target_feature
         products[product_code] = stock_price
     return products
