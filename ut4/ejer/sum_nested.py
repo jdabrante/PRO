977d99fae808d5e9ced377e0839da09bfcec02e7
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2023-03-10 00:24:09 +0100

    Se sube ejercicio resuelto de sum_nested

--- ut4/ejer/sum_nested.py
+++ ut4/ejer/sum_nested.py
@@ -3,12 +3,15 @@
 # ***********************
 
 
-def sum_nested(items: list, sum=0):
+def sum_nested(items: list, suma=0):
     if len(items) == 0:
-        return sum
+        return suma 
     if isinstance(items[0], int):
-        sum += items[0]
-        if len(items) == 1:
-            return sum
-    else:
-        return sum_nested(items[1:], sum)
+        suma += items[0]
+        return sum_nested(items[1:], suma)
+    else: 
+        for item in items[0]:
+            items.append(item)
+        del items[0]
+        return sum_nested(items, suma)
+
