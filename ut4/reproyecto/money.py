df7bd3322ff7481aadff093de935fa585d7d8143
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2023-03-11 03:19:13 +0100

    Se sube lo del proyecto

--- ut4/reproyecto/money.py
+++
@@ -1,9 +0,0 @@
-def rest_money (operation: list, money: int) -> int:
-    money += int(operation[1])
-    return money
-
-
-
-
-
-
--- ut4/reproyecto/read.py
+++
@@ -1,5 +0,0 @@
-def read2 (path: str)-> list: 
-    with open(path) as f:
-        operations = [ line.strip().split() for line in f]
-    return operations
-    
